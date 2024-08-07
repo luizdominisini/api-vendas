39597b9bf2b7145bfbe2b5411c27c559f2c5e472
Luiz Gustavo Dominisini <dominizi.luiz@gmail.com>
2024-08-07 15:01:51 -0300

    Fazer migracao tabela order_products

---
+++ src/shared/typeorm/migrations/1723053652739-CreateOrdersProducts.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateOrdersProducts1723053652739 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
